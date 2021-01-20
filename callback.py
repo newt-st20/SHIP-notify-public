089013efc620ce33602495657aa0b901472981f2
NEWT <ysti2019.win2020.st@gmail.com>
2021-01-20 20:38:20 +0900

    000...の除外処理

--- callback.py
+++ callback.py
@@ -69,6 +69,8 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    if event.reply_token == "00000000000000000000000000000000":
+        return
     line_bot_api.reply_message(
         event.reply_token, TextSendMessage(text="読込中..."))
     getlogsSheet = gc.open_by_key(SPREADSHEET_KEY).worksheet('getlogs')
