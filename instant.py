06212d5df8cf866d77db40ddfa19c886063f5c5a
NEWT <ysti2019.win2020.st@gmail.com>
2021-01-29 17:02:14 +0900

    保存のシワスレ

--- instant.py
+++ instant.py
@@ -31,7 +31,7 @@ def main():
     jsonOpen = open('other.json', 'r', encoding="utf-8_sig")
     jsonLoad = json.load(jsonOpen)
     broadcastEndPoint = "https://api.line.me/v2/bot/message/broadcast"
-    jsonData = jsonLoad['instantNormal']
+    jsonData = jsonLoad['instantWithFewMenu']
     jsonData['messages'][0]['text'] = "【2月からの機能追加・仕様変更について】\n・連絡事項の説明欄の文も取得するように\n・深夜と早朝は返事が遅くなることがある\n・通知の設定の種類が3種類に増えた\n https://newt-house.web.app/ship-notify/#release-note"
     print(jsonData)
     requests.post(broadcastEndPoint, json=jsonData, headers=headers)
