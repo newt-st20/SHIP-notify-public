e8158343d3c699f01f5f9b55e516f7507c962607
newt239 <ysti2019.win2020.st@gmail.com>
2021-03-07 19:42:57 +0900

    高校のページのデータを取得する処理の追加

--- discordbot.py
+++ discordbot.py
@@ -47,8 +47,18 @@ async def on_message(message):
     elif 'sh!' in message.content:
         if message.content == 'sh!':
             await message.channel.send('`sh!`はコマンドです。')
+        elif 'help' in message.content:
+            await message.channel.send('ヘルプ')
+        elif 'search' in message.content:
+            await message.channel.send('検索')
         elif 'neko' in message.content:
             await message.channel.send('にゃーん')
+        elif 'get' in message.content and message.author.guild_permissions.administrator:
+            await message.channel.send('データの取得を開始...')
+            try:
+                await getData()
+            except Exception as e:
+                await message.channel.send('エラータイプ:' + str(type(e))+'\nエラーメッセージ:' + e.message)
         else:
             await message.channel.send('このコマンドは用意されていません')
     if isinstance(message.channel, discord.DMChannel):
@@ -101,13 +111,13 @@ async def loop():
     getLogChannel = client.get_channel(817400535639916544)
     nowHour = int(datetime.datetime.now().strftime("%H"))
     nowMinute = int(datetime.datetime.now().strftime("%M"))
-    print(nowMinute)
     if nowHour % 6 == 0 and nowMinute < 10:
+        await getLogChannel.send('データの取得を開始...')
         result = sqlpush.main()
         if len(result[0]) != 0:
             for conData in result[0]:
                 embed = discord.Embed(
-                    title="連絡事項更新通知", description="取得:"+result[2], color=discord.Colour.from_rgb(52, 235, 79))
+                    title="連絡事項更新通知", description="取得日時:"+result[4], color=discord.Colour.from_rgb(52, 235, 79))
                 embed.add_field(name="date", value=conData[0], inline=False)
                 embed.add_field(name="path", value=conData[1], inline=False)
                 embed.add_field(name="title", value=conData[2], inline=False)
@@ -117,18 +127,125 @@ async def loop():
                     print("no data")
                 await conJuniorChannel.send(embed=embed)
         else:
-            await getLogChannel.send('連絡事項に更新はありませんでした')
+            embed = discord.Embed(
+                title="中学連絡事項更新通知", description="取得日時:"+result[4], color=discord.Colour.from_rgb(52, 235, 79))
+            embed.add_field(name="system-log",
+                            value='中学連絡事項に更新はありませんでした', inline=False)
+            await getLogChannel.send(embed=embed)
         if len(result[1]) != 0:
             for studyData in result[1]:
                 embed = discord.Embed(
-                    title="学習教材更新通知", description="取得:"+result[2], color=discord.Colour.from_rgb(52, 229, 235))
+                    title="中学学習教材更新通知", description="取得:"+result[4], color=discord.Colour.from_rgb(52, 229, 235))
                 embed.add_field(name="date", value=studyData[0], inline=False)
                 embed.add_field(name="path", value=studyData[1], inline=False)
                 embed.add_field(name="title", value=studyData[2], inline=False)
                 await studyJuniorChannel.send(embed=embed)
         else:
-            await getLogChannel.send('学習教材に更新はありませんでした')
+            embed = discord.Embed(
+                title="中学学習教材更新通知", description="取得日時:"+result[4], color=discord.Colour.from_rgb(52, 235, 79))
+            embed.add_field(name="system-log",
+                            value='中学学習教材に更新はありませんでした', inline=False)
+            await getLogChannel.send(embed=embed)
+        if len(result[2]) != 0:
+            for conData in result[2]:
+                embed = discord.Embed(
+                    title="高校連絡事項更新通知", description="取得日時:"+result[4], color=discord.Colour.from_rgb(52, 235, 79))
+                embed.add_field(name="date", value=conData[0], inline=False)
+                embed.add_field(name="path", value=conData[1], inline=False)
+                embed.add_field(name="title", value=conData[2], inline=False)
+                try:
+                    embed.add_field(name="description", value=conData[3])
+                except:
+                    print("no data")
+                await conHighChannel.send(embed=embed)
+        else:
+            embed = discord.Embed(
+                title="高校連絡事項更新通知", description="取得日時:"+result[4], color=discord.Colour.from_rgb(52, 235, 79))
+            embed.add_field(name="system-log",
+                            value='高校連絡事項に更新はありませんでした', inline=False)
+            await getLogChannel.send(embed=embed)
+        if len(result[3]) != 0:
+            for studyData in result[3]:
+                embed = discord.Embed(
+                    title="高校学習教材更新通知", description="取得:"+result[4], color=discord.Colour.from_rgb(52, 229, 235))
+                embed.add_field(name="date", value=studyData[0], inline=False)
+                embed.add_field(name="path", value=studyData[1], inline=False)
+                embed.add_field(name="title", value=studyData[2], inline=False)
+                await studyHighChannel.send(embed=embed)
+        else:
+            embed = discord.Embed(
+                title="高校学習教材更新通知", description="取得日時:"+result[4], color=discord.Colour.from_rgb(52, 235, 79))
+            embed.add_field(name="system-log",
+                            value='高校学習教材に更新はありませんでした', inline=False)
+            await getLogChannel.send(embed=embed)
 
+
+async def getData():
+    result = sqlpush.main()
+    if len(result[0]) != 0:
+        for conData in result[0]:
+            embed = discord.Embed(
+                title="連絡事項更新通知", description="取得日時:"+result[4], color=discord.Colour.from_rgb(52, 235, 79))
+            embed.add_field(name="date", value=conData[0], inline=False)
+            embed.add_field(name="path", value=conData[1], inline=False)
+            embed.add_field(name="title", value=conData[2], inline=False)
+            try:
+                embed.add_field(name="description", value=conData[3])
+            except:
+                print("no data")
+            await conJuniorChannel.send(embed=embed)
+    else:
+        embed = discord.Embed(
+            title="中学連絡事項更新通知", description="取得日時:"+result[4], color=discord.Colour.from_rgb(52, 235, 79))
+        embed.add_field(name="system-log",
+                        value='中学連絡事項に更新はありませんでした', inline=False)
+        await getLogChannel.send(embed=embed)
+    if len(result[1]) != 0:
+        for studyData in result[1]:
+            embed = discord.Embed(
+                title="中学学習教材更新通知", description="取得:"+result[4], color=discord.Colour.from_rgb(52, 229, 235))
+            embed.add_field(name="date", value=studyData[0], inline=False)
+            embed.add_field(name="path", value=studyData[1], inline=False)
+            embed.add_field(name="title", value=studyData[2], inline=False)
+            await studyJuniorChannel.send(embed=embed)
+    else:
+        embed = discord.Embed(
+            title="中学学習教材更新通知", description="取得日時:"+result[4], color=discord.Colour.from_rgb(52, 235, 79))
+        embed.add_field(name="system-log",
+                        value='中学学習教材に更新はありませんでした', inline=False)
+        await getLogChannel.send(embed=embed)
+    if len(result[2]) != 0:
+        for conData in result[2]:
+            embed = discord.Embed(
+                title="高校連絡事項更新通知", description="取得日時:"+result[4], color=discord.Colour.from_rgb(52, 235, 79))
+            embed.add_field(name="date", value=conData[0], inline=False)
+            embed.add_field(name="path", value=conData[1], inline=False)
+            embed.add_field(name="title", value=conData[2], inline=False)
+            try:
+                embed.add_field(name="description", value=conData[3])
+            except:
+                print("no data")
+            await conHighChannel.send(embed=embed)
+    else:
+        embed = discord.Embed(
+            title="高校連絡事項更新通知", description="取得日時:"+result[4], color=discord.Colour.from_rgb(52, 235, 79))
+        embed.add_field(name="system-log",
+                        value='高校連絡事項に更新はありませんでした', inline=False)
+        await getLogChannel.send(embed=embed)
+    if len(result[3]) != 0:
+        for studyData in result[3]:
+            embed = discord.Embed(
+                title="高校学習教材更新通知", description="取得:"+result[4], color=discord.Colour.from_rgb(52, 229, 235))
+            embed.add_field(name="date", value=studyData[0], inline=False)
+            embed.add_field(name="path", value=studyData[1], inline=False)
+            embed.add_field(name="title", value=studyData[2], inline=False)
+            await studyHighChannel.send(embed=embed)
+    else:
+        embed = discord.Embed(
+            title="高校学習教材更新通知", description="取得日時:"+result[4], color=discord.Colour.from_rgb(52, 235, 79))
+        embed.add_field(name="system-log",
+                        value='高校学習教材に更新はありませんでした', inline=False)
+        await getLogChannel.send(embed=embed)
 
 loop.start()
 
--- sqlpush.py
+++ sqlpush.py
@@ -42,85 +42,110 @@ def main():
     time.sleep(getWaitSecs())
     ship_id = driver.find_element_by_name("ship_id")
     password = driver.find_element_by_name("pass")
-    ship_id.send_keys('shj181152')
-    password.send_keys('ry9k5btx')
+    ship_id.send_keys(os.environ['SHIP_ID'])
+    password.send_keys(os.environ['SHIP_PASS'])
     driver.find_element_by_name('login').click()
     time.sleep(getWaitSecs())
-    driver.get("https://ship.sakae-higashi.jp/connection/search.php?obj_id=&depth=&search=&s_y=2011&s_m=01&s_d=01&e_y=2030&e_m=12&e_d=31")
-    con = driver.page_source
-    conSoup = BeautifulSoup(con, 'html.parser')
-    conLinks = conSoup.find_all(class_='allc')[0].find_all('a')
-    conEachPage = []
-    for conLink in conLinks:
-        conOnclick = conLink.get('onclick')
-        conId = re.findall("'([^']*)'", conOnclick)[0]
-        time.sleep(getWaitSecs())
+    count = 0
+    while count < 2:
+        driver.get("https://ship.sakae-higashi.jp/menu.php")
+        menu = driver.page_source
+        menuSoup = BeautifulSoup(menu, 'html.parser')
+        menuStatus = menuSoup.find_all('table')[1].text
+        menuBtn = menuSoup.find_all('table')[2].find_all('input')[1]
+        if count == 0 and '中学校' in menuStatus:
+            menuBtn.click()
+        elif count == 1 and '高等学校' in menuStatus:
+            menuBtn.click()
         driver.get(
-            "https://ship.sakae-higashi.jp/sub_window_anke/?obj_id="+conId+"&t=3")
-        conEachPage.append(driver.page_source)
+            "https://ship.sakae-higashi.jp/connection/search.php?obj_id=&depth=&search=&s_y=2011&s_m=01&s_d=01&e_y=2030&e_m=12&e_d=31")
+        con = driver.page_source
+        conSoup = BeautifulSoup(con, 'html.parser')
+        conLinks = conSoup.find_all(class_='allc')[0].find_all('a')
+        conEachPage = []
+        for conLink in conLinks:
+            conOnclick = conLink.get('onclick')
+            conId = re.findall("'([^']*)'", conOnclick)[0]
+            time.sleep(getWaitSecs())
+            driver.get(
+                "https://ship.sakae-higashi.jp/sub_window_anke/?obj_id="+conId+"&t=3")
+            conEachPage.append(driver.page_source)
 
-    driver.get("https://ship.sakae-higashi.jp/study/search.php?obj_id=&depth=&search=&s_y=2011&s_m=01&s_d=01&e_y=2030&e_m=12&e_d=31")
-    study = driver.page_source
-    studySoup = BeautifulSoup(study, 'html.parser')
+        driver.get(
+            "https://ship.sakae-higashi.jp/study/search.php?obj_id=&depth=&search=&s_y=2011&s_m=01&s_d=01&e_y=2030&e_m=12&e_d=31")
+        study = driver.page_source
+        studySoup = BeautifulSoup(study, 'html.parser')
+
+        conBody = conSoup.find("body")
+        conTrs = conSoup.find_all(class_='allc')[0].find_all('tr')
+        conTrs.pop(0)
+        conList = []
+        conc = 0
+        for conTr in conTrs:
+            eachconList = []
+            conTrTds = conTr.find_all('td')
+            try:
+                stage = conTrTds[2].find('a').get('onclick')
+                eachconList.append(re.findall("'([^']*)'", stage))
+            except:
+                eachconList.append([0, 0])
+            eachconList.append(conTrTds[0].text)
+            try:
+                eachconList.append(conTrTds[1].find('span').get('title'))
+            except:
+                eachconList.append(conTrTds[1].text)
+            eachconList.append(conTrTds[2].text.replace("\n", ""))
+            conEachPageSoup = BeautifulSoup(conEachPage[conc], 'html.parser')
+            conPageMain = conEachPageSoup.find_all(
+                class_='ac')[0].find_all("table")[1]
+            conPageDescription = conPageMain.find_all(
+                "table")[-2].text.replace("\n", "")
+            eachconList.append(conPageDescription)
+            conList.append(eachconList)
+            conc += 1
+        print(conList)
+
+        studyBody = studySoup.find("body")
+        studyTrs = studySoup.find_all(class_='allc')[0].find_all('tr')
+        studyTrs.pop(0)
+        studyList = []
+        studyc = 0
+        for studyTr in studyTrs:
+            eachstudyList = []
+            studyTrTds = studyTr.find_all('td')
+            try:
+                stage = studyTrTds[2].find('a').get('onclick')
+                eachstudyList.append(re.findall("'([^']*)'", stage))
+            except:
+                eachstudyList.append([0, 0])
+            eachstudyList.append(studyTrTds[0].text)
+            try:
+                eachstudyList.append(studyTrTds[1].find('span').get('title'))
+            except:
+                eachstudyList.append(studyTrTds[1].text)
+            eachstudyList.append(studyTrTds[2].text.replace("\n", ""))
+            studyList.append(eachstudyList)
+            studyc += 1
+        print(studyList)
+
+        driver.get("https://ship.sakae-higashi.jp/menu.php")
+        menu = driver.page_source
+        menuSoup = BeautifulSoup(menu, 'html.parser')
+        menuStatus = menuSoup.find_all('table')[1].text
+        if '中学校' in menuStatus:
+            juniorConList = conList
+            juniorStudyList = studyList
+        elif '高等学校' in menuStatus:
+            highConList = conList
+            highStudyList = studyList
+        count += 1
     driver.quit()
-
-    conBody = conSoup.find("body")
-    conTrs = conSoup.find_all(class_='allc')[0].find_all('tr')
-    conTrs.pop(0)
-    conList = []
-    conc = 0
-    for conTr in conTrs:
-        eachconList = []
-        conTrTds = conTr.find_all('td')
-        try:
-            stage = conTrTds[2].find('a').get('onclick')
-            eachconList.append(re.findall("'([^']*)'", stage))
-        except:
-            eachconList.append([0, 0])
-        eachconList.append(conTrTds[0].text)
-        try:
-            eachconList.append(conTrTds[1].find('span').get('title'))
-        except:
-            eachconList.append(conTrTds[1].text)
-        eachconList.append(conTrTds[2].text.replace("\n", ""))
-        conEachPageSoup = BeautifulSoup(conEachPage[conc], 'html.parser')
-        conPageMain = conEachPageSoup.find_all(
-            class_='ac')[0].find_all("table")[1]
-        conPageDescription = conPageMain.find_all(
-            "table")[-2].text.replace("\n", "")
-        eachconList.append(conPageDescription)
-        conList.append(eachconList)
-        conc += 1
-    print(conList)
-
-    studyBody = studySoup.find("body")
-    studyTrs = studySoup.find_all(class_='allc')[0].find_all('tr')
-    studyTrs.pop(0)
-    studyList = []
-    studyc = 0
-    for studyTr in studyTrs:
-        eachstudyList = []
-        studyTrTds = studyTr.find_all('td')
-        try:
-            stage = studyTrTds[2].find('a').get('onclick')
-            eachstudyList.append(re.findall("'([^']*)'", stage))
-        except:
-            eachstudyList.append([0, 0])
-        eachstudyList.append(studyTrTds[0].text)
-        try:
-            eachstudyList.append(studyTrTds[1].find('span').get('title'))
-        except:
-            eachstudyList.append(studyTrTds[1].text)
-        eachstudyList.append(studyTrTds[2].text.replace("\n", ""))
-        studyList.append(eachstudyList)
-        studyc += 1
-    print(studyList)
 
     with get_connection() as conn:
         with conn.cursor() as cur:
-            conSendData = []
-            studySendData = []
-            for i in conList:
+            juniorConSendData = []
+            juniorStudySendData = []
+            for i in juniorConList:
                 if i[0][0] != 0:
                     cur.execute('SELECT EXISTS (SELECT * FROM con_junior WHERE id = %s)',
                                 [int(i[0][0])])
@@ -130,8 +155,8 @@ def main():
                             "年", "/").replace("月", "/").replace("日", "")
                         cur.execute('INSERT INTO con_junior (id, date, folder, title, description) VALUES (%s, %s, %s, %s, %s)', [
                                     i[0][0], date, i[2], i[3], i[4]])
-                        conSendData.append([date, i[2], i[3], i[4]])
-            for i in studyList:
+                        juniorConSendData.append([date, i[2], i[3], i[4]])
+            for i in juniorStudyList:
                 if i[0][0] != 0:
                     cur.execute('SELECT EXISTS (SELECT * FROM study_junior WHERE id = %s)',
                                 [int(i[0][0])])
@@ -141,12 +166,38 @@ def main():
                             "年", "/").replace("月", "/").replace("日", "")
                         cur.execute('INSERT INTO study_junior (id, date, folder, title) VALUES (%s, %s, %s, %s)', [
                                     i[0][0], date, i[2], i[3]])
-                        studySendData.append([date, i[2], i[3]])
+                        juniorStudySendData.append([date, i[2], i[3]])
+            highConSendData = []
+            highStudySendData = []
+            for i in highConList:
+                if i[0][0] != 0:
+                    cur.execute('SELECT EXISTS (SELECT * FROM con_high WHERE id = %s)',
+                                [int(i[0][0])])
+                    (b,) = cur.fetchone()
+                    if b == False:
+                        date = i[1].replace(
+                            "年", "/").replace("月", "/").replace("日", "")
+                        cur.execute('INSERT INTO con_high (id, date, folder, title, description) VALUES (%s, %s, %s, %s, %s)', [
+                                    i[0][0], date, i[2], i[3], i[4]])
+                        highConSendData.append([date, i[2], i[3], i[4]])
+            for i in highStudyList:
+                if i[0][0] != 0:
+                    cur.execute('SELECT EXISTS (SELECT * FROM study_junior WHERE id = %s)',
+                                [int(i[0][0])])
+                    (b,) = cur.fetchone()
+                    if b == False:
+                        date = i[1].replace(
+                            "年", "/").replace("月", "/").replace("日", "")
+                        cur.execute('INSERT INTO study_high (id, date, folder, title) VALUES (%s, %s, %s, %s)', [
+                                    i[0][0], date, i[2], i[3]])
+                        highStudySendData.append([date, i[2], i[3]])
         conn.commit()
 
-    print(conSendData)
-    print(studySendData)
-    return conSendData, studySendData, getTime
+    print(juniorConSendData[::-1])
+    print(juniorStudySendData[::-1])
+    print(highConSendData[::-1])
+    print(highStudySendData[::-1])
+    return juniorConSendData[::-1], juniorStudySendData[::-1], highConSendData[::-1], highStudySendData[::-1], getTime
 
 
 def get_connection():
