1efceb79392de6aa2ef144c462b4823977ce4770
newt239 <ysti2019.win2020.st@gmail.com>
2021-03-08 19:23:39 +0900

    discordの定期データ取得のバグ修正、LINE側との最適化

--- push.py
+++ push.py
@@ -43,147 +43,22 @@ else:
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 
 
-def main():
-    now = datetime.datetime.now()
-    getTime = now.strftime('%Y/%m/%d %H:%M:%S')
-    driver_path = '/app/.chromedriver/bin/chromedriver'
-    options = Options()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--disable-extensions')
-    options.add_argument('--proxy-server="direct://"')
-    options.add_argument('--proxy-bypass-list=*')
-    options.add_argument('--start-maximized')
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(executable_path=driver_path, options=options)
-    driver.get('https://ship.sakae-higashi.jp/')
-    time.sleep(getWaitSecs())
-    ship_id = driver.find_element_by_name("ship_id")
-    password = driver.find_element_by_name("pass")
-    ship_id.send_keys(os.environ['SHIP_ID'])
-    password.send_keys(os.environ['SHIP_PASS'])
-    driver.find_element_by_name('login').click()
-    time.sleep(getWaitSecs())
-    driver.get("https://ship.sakae-higashi.jp/connection/search.php?obj_id=&depth=&search=&s_y=2011&s_m=01&s_d=01&e_y=2030&e_m=12&e_d=31")
-    con = driver.page_source
-    conSoup = BeautifulSoup(con, 'html.parser')
-    conSource = conSoup.find("body")
-    conTable = conSoup.find_all(class_='allc')[0]
-    conLinks = conTable.find_all('a')
-    conTrs = conTable.find_all("tr")
-    conDataLists = []
-    for conTr in conTrs:
-        conDataList = []
-        conTds = conTr.find_all("td")
-        for conTd in conTds:
-            conDataList.append(conTd.text)
-        conDataLists.append(conDataList)
-    conDataLists.pop(0)
-    conPageDescriptions = []
-    for conLink in conLinks:
-        conOnclick = conLink.attrs['onclick']
-        conLeft = conOnclick.find("'")
-        conRight = conOnclick.find("'", conLeft+1)
-        conId = format(conOnclick[conLeft+1:conRight])
-        time.sleep(getWaitSecs())
-        driver.get(
-            "https://ship.sakae-higashi.jp/sub_window_anke/?obj_id="+conId+"&t=3")
-        conEachPage = driver.page_source
-        conEachPageSoup = BeautifulSoup(conEachPage, 'html.parser')
-        conPageMain = conEachPageSoup.find_all(
-            class_='ac')[0].find_all("table")[1]
-        conPageDescription = conPageMain.find_all("table")[-2].text
-        conPageDescriptions.append(conPageDescription)
-    conAllDataCounter = 0
-    conAllDataList = []
-    for conData in range(len(conDataLists)):
-        conDatas = []
-        conDatas.append(conDataLists[conAllDataCounter][0])
-        conDatas.append(conDataLists[conAllDataCounter][1])
-        conDatas.append(conDataLists[conAllDataCounter][2])
-        conDatas.append(conPageDescriptions[conAllDataCounter])
-        conAllDataList.append(conDatas)
-        conAllDataCounter += 1
-    print(conAllDataList)
-    driver.get("https://ship.sakae-higashi.jp/study/search.php?obj_id=&depth=&search=&s_y=2011&s_m=01&s_d=01&e_y=2030&e_m=12&e_d=31")
-    study = driver.page_source
-    driver.quit()
-
-    studySoup = BeautifulSoup(study, 'html.parser')
-    studySource = studySoup.find("body")
-    studyText = studySoup.find_all(class_='allc')[0].find_all('tr')
-    studyList = []
-    for i in range(len(studySoup.find_all(class_='allc')[0].find_all('tr'))):
-        for j in range(len(studySoup.find_all(class_='allc')[0].find_all('tr')[i].find_all('td')[0])):
-            eachStudyList = []
-            eachStudyList.append(studySoup.find_all(class_='allc')[
-                0].find_all('tr')[i].find_all('td')[0].text)
-            try:
-                eachStudyList.append(studySoup.find_all(class_='allc')[0].find_all('tr')[
-                    i].find_all('td')[1].find('span').get('title'))
-            except:
-                eachStudyList.append(studySoup.find_all(class_='allc')[
-                    0].find_all('tr')[i].find_all('td')[1].text)
-            eachStudyList.append(studySoup.find_all(class_='allc')[
-                0].find_all('tr')[i].find_all('td')[2].text)
-            studyList.append(eachStudyList)
-    studyList.pop(0)
-    print(studyList)
-
-    connectionSheet = gc.open_by_key(SPREADSHEET_KEY).worksheet('connection')
-    connectionSheetLow = len(connectionSheet.col_values(1))
-    connectionData = [str(conSource), str(conAllDataList), getTime]
-    connectionOldData = connectionSheet.cell(connectionSheetLow, 2).value
+def main(conSendData, studySendData, getTime):
     message1 = ""
     try:
-        conFlex0 = conFlex1 = conFlex2 = conFlex3 = []
-        if connectionOldData != str(conAllDataList):
-            connectionSheet.append_row(connectionData)
-            for a in conAllDataList:
-                if str(a) in str(connectionOldData):
-                    pass
-                else:
-                    message1 += "\n・連絡事項:" + \
-                        a[0] + "-" + a[1] + "-" + \
-                        a[2].replace("\n", "")
-                    if a[3].replace("\n", "") != "":
-                        message1 += "\n《" + a[3].replace("\n", "") + "》"
-                    conFlex0.append(a[0])
-                    conFlex1.append(a[1])
-                    conFlex2.append(a[2])
-                    conFlex3.append(a[3])
-        else:
-            connectionSheet.update_cell(connectionSheetLow, 4, getTime)
-            message1 = "\n・連絡事項:更新はありません"
+        for a in conSendData:
+            message1 += "\n・連絡事項:" + a[0] + "-" + a[1] + "-" + a[2]
+            if a[3] != "":
+                message1 += "\n《" + a[3] + "》"
     except:
         message1 = "\n・連絡事項取得エラー:更新の有無を取得できませんでした"
         import traceback
         traceback.print_exc()
 
-    studySheet = gc.open_by_key(SPREADSHEET_KEY).worksheet('study')
-    studySheetLow = len(studySheet.col_values(1))
-    studyData = [str(studySource), str(studyList), getTime]
-    studyOldData = studySheet.cell(studySheetLow, 2).value
-    studyNewData = studyList
-    message2 = "\n"
+    message2 = ""
     try:
-        if studyOldData != str(studyList):
-            studySheet.append_row(studyData)
-            for b in studyList:
-                print(str(b))
-                if str(b) in str(studyOldData):
-                    pass
-                else:
-                    message2 += "\n・学習教材:" + b[0] + "-" + \
-                        b[1] + "-"
-                    if b[2].replace("\n", " ") != "":
-                        message2 += b[2].replace("\n", " ")
-                    studyFlex0 = studyFlex1 = studyFlex2 = []
-                    studyFlex0.append(b[0])
-                    studyFlex1.append(b[1])
-                    studyFlex2.append(b[2])
-        else:
-            studySheet.update_cell(studySheetLow, 4, getTime)
-            message2 = "\n・学習教材:更新はありません"
+        for b in studySendData:
+            message2 += "\n・学習教材:" + b[0] + "-" + b[1] + "-" + b[2]
     except:
         message2 = "\n・学習教材取得エラー:更新の有無を取得できませんでした"
         import traceback
@@ -199,55 +74,30 @@ def main():
     useridSheetLow = len(useridSheet.col_values(1))
     reportSheet = gc.open_by_key(SPREADSHEET_KEY).worksheet('report')
     reportSheetLow = len(reportSheet.col_values(1))
-    mail = "【" + getTime.replace("-", "/") + "】\n" + message1 + message2
-    if "連絡事項:更新はありません" in message1 and "学習教材:更新はありません" in message2:
-        multicastEndPoint = "https://api.line.me/v2/bot/message/multicast"
-        jsonAllData = jsonLoad['pushForNotifyAllUser']
-        jsonAllData['messages'][0]['text'] = mail
-        finalAllSendList = findUser(5, "notify-all")
-        jsonAllData['to'] = finalAllSendList
-        requests.post(multicastEndPoint, json=jsonAllData, headers=headers)
-        num = random.randrange(10)
-        logMessage = "send message:" + \
-            str(mail)+" send for:" + str(finalAllSendList) + \
-            ". Random number is " + str(num)
-        if num == 0:
-            # middle
-            jsonMiddleData = jsonLoad['pushForNotifyMiddleUser']
-            finalMiddleSendList = findUser(5, "notify-middle")
-            jsonMiddleData['to'] = finalMiddleSendList
-            requests.post(multicastEndPoint,
-                          json=jsonMiddleData, headers=headers)
-            logMessage = "send message:" + \
-                str(mail)+"[all] send for:" + str(finalAllSendList) + ",[middle] send for:" + str(finalMiddleSendList) + \
-                ". Random number is " + str(num)
-        print(logMessage)
-        reportSheet.update_cell(reportSheetLow+1, 1,
-                                logMessage.replace("\n", ""))
-    else:
-        broadcastEndPoint = "https://api.line.me/v2/bot/message/broadcast"
-        jsonData = jsonLoad['default']
-        jsonData['messages'][0]['text'] = mail
-        print(jsonData)
-        requests.post(broadcastEndPoint, json=jsonData, headers=headers)
-        logMessage = "send message:" + \
-            str(mail)+" send for all followed user."
-        print(logMessage)
-        reportSheet.update_cell(reportSheetLow+1, 1,
-                                logMessage.replace("\n", ""))
-        # beta-user
-        multicastEndPoint = "https://api.line.me/v2/bot/message/multicast"
-        jsonData = jsonLoad['pushForAll']
-        BetaSendList = findUser(6, "beta-on")
-        jsonData['to'] = BetaSendList
-        jsonData['messages'][0]['contents']['header']['contents'][1]['text'] = "最終取得: " + \
-            getTime.replace("-", "/")
-        jsonData['messages'][0]['contents']['body']['contents'][1]['text'] = message1.replace(
-            "連絡事項:", "")
-        jsonData['messages'][0]['contents']['body']['contents'][4]['text'] = message2.replace(
-            "学習教材:", "")
-        print(jsonData)
-        requests.post(multicastEndPoint, json=jsonData, headers=headers)
+    mail = "【" + getTime + "】\n" + message1 + message2
+    broadcastEndPoint = "https://api.line.me/v2/bot/message/broadcast"
+    jsonData = jsonLoad['default']
+    jsonData['messages'][0]['text'] = mail
+    print(jsonData)
+    requests.post(broadcastEndPoint, json=jsonData, headers=headers)
+    logMessage = "send message:" + \
+        str(mail)+" send for all followed user."
+    print(logMessage)
+    reportSheet.update_cell(reportSheetLow+1, 1,
+                            logMessage.replace("\n", ""))
+    # beta-user
+    multicastEndPoint = "https://api.line.me/v2/bot/message/multicast"
+    jsonData = jsonLoad['pushForAll']
+    BetaSendList = findUser(6, "beta-on")
+    jsonData['to'] = BetaSendList
+    jsonData['messages'][0]['contents']['header']['contents'][1]['text'] = "最終取得: " + \
+        getTime.replace("-", "/")
+    jsonData['messages'][0]['contents']['body']['contents'][1]['text'] = message1.replace(
+        "連絡事項:", "")
+    jsonData['messages'][0]['contents']['body']['contents'][4]['text'] = message2.replace(
+        "学習教材:", "")
+    print(jsonData)
+    requests.post(multicastEndPoint, json=jsonData, headers=headers)
 
 
 def getWaitSecs():
