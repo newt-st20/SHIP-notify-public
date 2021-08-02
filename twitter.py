1b2f84c283784a85652d38a60831597dccebd02c
newt239 <ysti2019.win2020.st@gmail.com>
2021-08-02 11:11:36 +0900

    add: twitterのリンクにga用のパラメータを付与

--- twitter.py
+++ twitter.py
@@ -3,7 +3,7 @@ from requests_oauthlib import OAuth1
 import requests
 import os
 from dotenv import load_dotenv
-import urllib
+import datetime
 
 load_dotenv()
 
@@ -14,9 +14,11 @@ ACCESS_TOKEN_SECRET = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
 
 
 def main(logid, updateList):
+    now = datetime.datetime.now()
+    date = now.strftime("%y%m%d")
     endpoint = "https://api.twitter.com/1.1/statuses/update.json"
     parameter = {
-        "status": str(updateList)+"に更新がありました。\n https://ship-assistant.web.app/log/"+str(logid)
+        "status": str(updateList)+"に更新がありました。\n https://ship-assistant.web.app/log/"+str(logid)+"?utm_source=tw_"+date+"&utm_medium=twitter"
     }
     auth = OAuth1(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
     response = requests.post(endpoint, params=parameter, auth=auth).json()
