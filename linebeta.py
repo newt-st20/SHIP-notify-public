da48da6c0ba4faa3ef19d7f4adc2a873408e09a7
newt239 <ysti2019.win2020.st@gmail.com>
2021-08-06 17:34:11 +0900

    update: linebetaのコード整理

--- linebeta.py
+++ linebeta.py
@@ -20,41 +20,49 @@ def main(data):
     jsonData = jsonLoad['flexMessage']
     jsonData['messages'][0]['contents']['body']['contents'][2]['text'] = "getTime: " + getTime
     separate = jsonLoad["separate"]
+
     highConData = data['highCon']
     if len(highConData) != 0:
         highConMessage = copy.deepcopy(jsonLoad['channelHead'])
         highConMessage['contents'][0]['text'] = "高校連絡事項"
+        props = json.load(open('json/ship.json', 'r', encoding="utf-8_sig"))['pageList'][0]['lineProps']
         for a in highConData:
             highConMessage['contents'].append(separate)
-            jsonEachMenuDate = copy.deepcopy(jsonLoad["eachMenuDate"])
-            jsonEachMenuDate["contents"][1]["text"] = a["date"]
-            highConMessage['contents'].append(jsonEachMenuDate)
-            jsonEachMenuFolder = copy.deepcopy(jsonLoad["eachMenuFolder"])
-            jsonEachMenuFolder["contents"][1]["text"] = a["folder"] if a["folder"]!="" else "root"
-            highConMessage['contents'].append(jsonEachMenuFolder)
-            jsonEachMenuTitle = copy.deepcopy(jsonLoad["eachMenuTitle"])
-            jsonEachMenuTitle["contents"][1]["text"] = a["title"]
-            highConMessage['contents'].append(jsonEachMenuTitle)
-            jsonEachMenuDescription = copy.deepcopy(jsonLoad["eachMenuDescription"])
-            jsonEachMenuDescription["contents"][1]["text"] = a["description"]
-            highConMessage['contents'].append(jsonEachMenuDescription)
+            for prop in props:
+                jsonEachMenu = copy.deepcopy(jsonLoad["eachMenu"])
+                jsonEachMenu["contents"][0]["text"] = prop
+                jsonEachMenu["contents"][1]["text"] = a[prop] if a[prop]!="" else "(empty)"
+                highConMessage['contents'].append(jsonEachMenu)
         jsonData['messages'][0]['contents']['body']['contents'].append(highConMessage)
+    
     highStudyData = data['highStudy']
     if len(highStudyData) != 0:
         highStudyMessage = copy.deepcopy(jsonLoad['channelHead'])
         highStudyMessage['contents'][0]['text'] = "高校学習教材"
+        props = json.load(open('json/ship.json', 'r', encoding="utf-8_sig"))['pageList'][1]['lineProps']
         for a in highStudyData:
             highStudyMessage['contents'].append(separate)
-            jsonEachMenuDate = copy.deepcopy(jsonLoad["eachMenuDate"])
-            jsonEachMenuDate["contents"][1]["text"] = a["date"]
-            highStudyMessage['contents'].append(jsonEachMenuDate)
-            jsonEachMenuFolder = copy.deepcopy(jsonLoad["eachMenuFolder"])
-            jsonEachMenuFolder["contents"][1]["text"] = a["folder"] if a["folder"]!="" else "root"
-            highStudyMessage['contents'].append(jsonEachMenuFolder)
-            jsonEachMenuTitle = copy.deepcopy(jsonLoad["eachMenuTitle"])
-            jsonEachMenuTitle["contents"][1]["text"] = a["title"]
-            highStudyMessage['contents'].append(jsonEachMenuTitle)
+            for prop in props:
+                jsonEachMenu = copy.deepcopy(jsonLoad["eachMenu"])
+                jsonEachMenu["contents"][0]["text"] = prop
+                jsonEachMenu["contents"][1]["text"] = a[prop] if a[prop]!="" else "(empty)"
+                highStudyMessage['contents'].append(jsonEachMenu)
         jsonData['messages'][0]['contents']['body']['contents'].append(highStudyMessage)
+
+    highSchoolNewsData = data['highSchoolNews']
+    if len(highSchoolNewsData) != 0:
+        highSchoolNewsMessage = copy.deepcopy(jsonLoad['channelHead'])
+        highSchoolNewsMessage['contents'][0]['text'] = "高校学習教材"
+        props = json.load(open('json/ship.json', 'r', encoding="utf-8_sig"))['pageList'][2]['lineProps']
+        for a in highSchoolNewsData:
+            highSchoolNewsMessage['contents'].append(separate)
+            for prop in props:
+                jsonEachMenu = copy.deepcopy(jsonLoad["eachMenu"])
+                jsonEachMenu["contents"][0]["text"] = prop
+                jsonEachMenu["contents"][1]["text"] = a[prop] if a[prop]!="" else "(empty)"
+                highSchoolNewsMessage['contents'].append(jsonEachMenu)
+        jsonData['messages'][0]['contents']['body']['contents'].append(highSchoolNewsMessage)
+
     jsonData['messages'][0]['contents']['footer']['contents'][0]['action']['uri'] = "https://ship-assistant.web.app/log/"+data['logId']+"?utm_source=line_"+date+"&utm_medium=LINE"
 
     betaheaders = {
@@ -74,4 +82,4 @@ def main(data):
 
 
 if __name__ == "__main__":
-    main()
+    main({'getTime': '06:06:12', 'timestamp': 'xxxx', 'logId': '62QSd77AOgIT9VzfE2qA', 'highCon': [], 'highStudy': [{'id': ['32597', '7'], 'link': [], 'date': '2021/08/06', 'folder': '2021 SCC42（高3）/数学 富澤 答', 'title': '0806求積3（体積）'}], 'highSchoolNews': [], 'juniorCon': [], 'juniorStudy': [], 'juniorSchoolNews': []})
