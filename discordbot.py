37d2806f219d30510329811b2b6ae1a0c442fb5d
newt239 <ysti2019.win2020.st@gmail.com>
2021-09-29 08:26:30 +0900

    update: 天気予報チャンネルで予報に雨の文字列があるときのみ通知

--- discordbot.py
+++ discordbot.py
@@ -632,18 +632,19 @@ async def getWeather():
     response = requests.get("https://www.jma.go.jp/bosai/forecast/data/forecast/110000.json").json()[0]
     pops = response['timeSeries'][1]['areas'][1]['pops']
     timeDefines = response['timeSeries'][1]['timeDefines']
-    title = "埼玉県南部の天気 - " + response['reportDatetime'][8:13].replace("T","日") + "時発表\n"
-    day1 = response['timeSeries'][0]['areas'][1]['weathers'][0].replace("晴れ", "🌞晴れ").replace("くもり","☁くもり").replace("雨","☔雨").replace("雷", "⚡雷")
-    day2 = response['timeSeries'][0]['areas'][1]['weathers'][1].replace("晴れ", "🌞晴れ").replace("くもり","☁くもり").replace("雨","☔雨").replace("雷", "⚡雷")
-    body = "`" + response['timeSeries'][0]['timeDefines'][0][8:10] + "日:` `" + day1 + "`\n`" + response['timeSeries'][0]['timeDefines'][1][8:10] + "日:` `" + day2 + "`\n> 降水確率\n"
-    for (pop, timeDefine) in zip(pops, timeDefines):
-        icon = "🌧"*(int(pop)//10)+"➖"*(10-int(pop)//10)
-        body += "`" + timeDefine[8:13].replace("T","日") + "時` " + icon + pop + "%\n"
-    response = requests.get("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/110000.json").json()
-    if response['headlineText'] != "":
-        body += "> 埼玉県の天気概況\n" + response['headlineText'] + "\n"
-    embed = discord.Embed(title=title, description=body, color=discord.Colour.from_rgb(163, 212, 255))
-    await weatherChannel.send(embed=embed)
+    if "雨" in response['timeSeries'][0]['areas'][1]['weathers'][0] or "雨" in response['timeSeries'][0]['areas'][1]['weathers'][1]:
+        title = "埼玉県南部の天気 - " + response['reportDatetime'][8:13].replace("T","日") + "時発表\n"
+        day1 = response['timeSeries'][0]['areas'][1]['weathers'][0].replace("晴れ", "🌞晴れ").replace("くもり","☁くもり").replace("雨","☔雨").replace("雷", "⚡雷")
+        day2 = response['timeSeries'][0]['areas'][1]['weathers'][1].replace("晴れ", "🌞晴れ").replace("くもり","☁くもり").replace("雨","☔雨").replace("雷", "⚡雷")
+        body = "`" + response['timeSeries'][0]['timeDefines'][0][8:10] + "日:` `" + day1 + "`\n`" + response['timeSeries'][0]['timeDefines'][1][8:10] + "日:` `" + day2 + "`\n> 降水確率\n"
+        for (pop, timeDefine) in zip(pops, timeDefines):
+            icon = "🌧"*(int(pop)//10)+"➖"*(10-int(pop)//10)
+            body += "`" + timeDefine[8:13].replace("T","日") + "時` " + icon + pop + "%\n"
+        response = requests.get("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/110000.json").json()
+        if response['headlineText'] != "":
+            body += "> 埼玉県の天気概況\n" + response['headlineText'] + "\n"
+        embed = discord.Embed(title=title, description=body, color=discord.Colour.from_rgb(163, 212, 255))
+        await weatherChannel.send(embed=embed)
 
 
 loop.start()
