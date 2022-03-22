9609d502d90e9522de13b4c36bb4b6c78301e034
newt239 <ysti2019.win2020.st@gmail.com>
2022-03-22 22:20:26 +0900

    fix: pjsekai programs

--- pjsekai.py
+++ pjsekai.py
@@ -1,5 +1,4 @@
 import requests
-import json
 import datetime
 
 def main():
@@ -12,11 +11,10 @@ def main():
     }
     ranking = requests.get('https://api.sekai.best/event/live?region=jp').json()
     if ranking["status"] == "success":
-        message = ranking["data"]["eventRankings"][0]["timestamp"][:-5].replace("T"," ") + "のスコア"
-        print(datetime.datetime.fromtimestamp(event["end"]//1000) - datetime.datetime.strptime(ranking["data"]["eventRankings"][0]["timestamp"][:-5], "%Y-%m-%dT%H:%M:%S"))
-        message += "(あと:" + str((datetime.datetime.fromtimestamp(event["end"]//1000) - datetime.datetime.strptime(ranking["data"]["eventRankings"][0]["timestamp"][:-5], "%Y-%m-%dT%H:%M:%S"))) + ") \n"
+        message = str(datetime.datetime.strptime(ranking["data"]["eventRankings"][0]["timestamp"][:-5], "%Y-%m-%dT%H:%M:%S") + datetime.timedelta(hours=9)) + "のスコア"
+        message += "(あと:" + str((datetime.datetime.fromtimestamp(event["end"]//1000) - datetime.datetime.strptime(ranking["data"]["eventRankings"][0]["timestamp"][:-5], "%Y-%m-%dT%H:%M:%S")) - datetime.timedelta(hours=9)) + ") \n"
         maxlen = len(str(ranking["data"]["eventRankings"][0]["score"]))
-        for eachPlayer in ranking["data"]["eventRankings"]:
+        for eachPlayer in sorted(ranking["data"]["eventRankings"], key=lambda x: x["rank"]):
             message += "``" + "".join([" "]*(6 - len(str(eachPlayer["rank"])))) + str(eachPlayer["rank"]) + "``位:  ``" +  "".join([" "]*(maxlen - len(str(eachPlayer["score"])))) + str(eachPlayer["score"]) + "`` - " + eachPlayer["userName"] + "\n"
             print(message)
         return [message, event]
